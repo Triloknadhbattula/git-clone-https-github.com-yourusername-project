adce9e57cbd926a51f3b78acf31133f611b099af
Triloknadhbattula <triloknadhbattula05@gmail.com>
2026-02-12 16:45:57 +0530

    Create Triloknadh.java
    Added LoginTest.java for Selenium automation.

---
+++ Triloknadh.java
@@ -0,0 +1,10 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    @Test
+    public void testLogin() {
+        // Selenium code for login
+        Assert.assertTrue(true);
+    }
+}
